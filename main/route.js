af4656c805d9b45c3c6f389849f59b68a8385441
JunHao-k <koh.junhao@trentglobal.edu.sg>
2022-06-18 18:30:00 +0000

    Added routing using geocoding

--- main/route.js
+++ main/route.js
@@ -1,11 +1,20 @@
 
 let routeBtn = document.querySelector("#routeSubmit")
+let routingControl = null
+let removeRoute = () => {
+    if (routingControl !== null) {
+        map.removeControl(routingControl);
+        routingControl = null;
+    }
+}
 
 routeBtn.addEventListener('click' , async () => {
     
+    removeRoute()
+    
     let current = document.querySelector("#start").value
     let destination = document.querySelector("#end").value 
-
+    
     let [currentArr, destinationArr] = await Promise.all([geoCode("postcode" , current), geoCode("postcode" , destination)])
 
     console.log(currentArr)
@@ -16,16 +25,23 @@ routeBtn.addEventListener('click' , async () => {
     }
 
     let currentLatLng = currentArr[0].center
-    let destinationLatLng = currentArr[0].center
+    let destinationLatLng = destinationArr[0].center
 
-    // let routingControl = L.Routing.control({
-//     waypoints: [
-//         L.latLng(1.3521, 103.8198),
-//         L.latLng(1.284284 , 103.849579)
-//     ]
-// })
+    let [c_lat , c_lng] = [currentLatLng[1] , currentLatLng[0]]
+    let [d_lat , d_lng] = [destinationLatLng[1] , destinationLatLng[0]]
 
-// routingControl.addTo(map)
+    // console.log(c_lat)
+    // console.log(c_lng)
+    // console.log(d_lat)
+    // console.log(d_lng)
+    routingControl = L.Routing.control({
+    waypoints: [
+            L.latLng(c_lat, c_lng),
+            L.latLng(d_lat , d_lng)
+        ]
+    })
+
+    routingControl.addTo(map)
 })
 
 
