e3dc3b9ccb87870baec02ab9b35f542c9da45303
JunHao-k <koh.junhao@trentglobal.edu.sg>
2022-06-18 15:32:58 +0000

    Solved some bug

--- main/route.js
+++ main/route.js
@@ -6,31 +6,19 @@ routeBtn.addEventListener('click' , async () => {
     let current = document.querySelector("#start").value
     let destination = document.querySelector("#end").value 
 
-    console.log(current)
-    console.log(destination)
-
-    let currentArr = await geoCode("postcode" , current)
-    let destinationArr = await geoCode("postcode" , destination)
+    let [currentArr, destinationArr] = await Promise.all([geoCode("postcode" , current), geoCode("postcode" , destination)])
 
     console.log(currentArr)
     console.log(destinationArr)
 
-    // if(currentArr.length == 0 || destinationArr.length == 0){
-    //     alert("Please enter a valid postal code")
-    // }
+    if(currentArr.length == 0 || destinationArr.length == 0){
+        alert("Please enter a valid postal code")
+    }
 
-    // let currentLatLng = currentArr[0].center
-    // let destinationLatLng = currentArr[0].center
+    let currentLatLng = currentArr[0].center
+    let destinationLatLng = currentArr[0].center
 
-    // console.log(currentLatLng)
-    // console.log(destinationLatLng)
-})
-
-
-
-
-
-// let routingControl = L.Routing.control({
+    // let routingControl = L.Routing.control({
 //     waypoints: [
 //         L.latLng(1.3521, 103.8198),
 //         L.latLng(1.284284 , 103.849579)
@@ -38,5 +26,12 @@ routeBtn.addEventListener('click' , async () => {
 // })
 
 // routingControl.addTo(map)
+})
+
+
+
+
+
+
 
 
