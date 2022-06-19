b63c691d97ff826813c687899d002bb4e609d973
JunHao-k <koh.junhao@trentglobal.edu.sg>
2022-06-19 07:39:52 +0000

    Added route

--- main/route.js
+++ main/route.js
@@ -11,6 +11,9 @@ let removeRoute = () => {
 routeBtn.addEventListener('click' , async () => {
     
     removeRoute()
+
+    // let startIcon = generateIcon("../images/start.png")
+    // let destinationIcon = generateIcon("../images/finish.png")
     
     let current = document.querySelector("#start").value
     let destination = document.querySelector("#end").value 
@@ -31,12 +34,50 @@ routeBtn.addEventListener('click' , async () => {
     let [d_lat , d_lng] = [destinationLatLng[1] , destinationLatLng[0]]
 
     routingControl = L.Routing.control({
+    position: 'topleft',
+    lineOptions: {
+        styles: [
+            // { color: '#17a2b8', opacity: 1, weight: 5 }
+            {
+                color: 'blue',
+                opacity: 1,
+                weight: 7
+            }
+        ]
+    },
     waypoints: [
             L.latLng(c_lat, c_lng),
             L.latLng(d_lat , d_lng)
-        ]
+        ],
+        createMarker: function(i , start , n){
+            let marker_icon = null
+            
+            if(i == 0){
+                marker_icon = generateIcon("../images/start.png")
+                return L.marker(start.latLng, {
+                    draggable: true,
+                    bounceOnAdd: false,
+                    bounceOnAddOptions: {
+                        duration: 1000,
+                        height: 800,
+                    },
+                    icon: marker_icon // here pass the custom marker icon instance
+                });
+            }
+            else if(i == n - 1){
+                marker_icon = generateIcon("../images/finish.png")
+                return L.marker(start.latLng, {
+                    draggable: true,
+                    bounceOnAdd: false,
+                    bounceOnAddOptions: {
+                        duration: 1000,
+                        height: 800,
+                    },
+                    icon: marker_icon // here pass the custom marker icon instance
+                });
+            }
+        },
     })
-
     routingControl.addTo(map)
 })
 
